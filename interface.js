317b9fdc2abddd4d239405ad5edfe133056b46fd
bgrove1974 <bgroveben@gmail.com>
2015-11-17 09:00:30 -0800

    Get remaining 2 tests to pass

--- interface.js
+++ interface.js
@@ -13,6 +13,7 @@ exports.insert = function(db, doc, callback) {
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
 exports.byDirector = function(db, director, callback) {
-  // TODO: implement
-  callback(null, []);
+  db.collection('movies').find({director:director}).sort({title: 1}).toArray(function(err,result) {
+  callback(null, result);
+  });
 };
